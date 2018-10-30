24a85edecca5d83994249dbb83979e8128d6817f
WareJosephB <Ware.JosephB@Gmail.com>
2018-10-30 15:06:34 +0000

    for testing

--- src/main/java/advanced/Runner.java
+++ src/main/java/advanced/Runner.java
@@ -4,33 +4,25 @@ import java.util.Scanner;
 
 public class Runner {
 	
-	private Runner() {}
+
+	private Runner() {
+	}
+
 
 	public static void main(String[] args) {
 		System.out.println(
-				"Please enter either 'Delete {account number}', 'Add {firstname}, {secondname}' or 'Retrieve {account number}', or 'Quit'");
+				"Please enter either 'D(elete) {account number}', 'A(dd) {firstname}, {secondname}', 'R(etrieve) {account number}', 'J(SON output) [optional account number]' or 'Q(uit)'");
+		Scanner scanner = new Scanner(System.in);
+
 		while (true) {
-
-			Scanner sc = new Scanner(System.in);
-			String input = sc.nextLine();
-			if ("delete".equalsIgnoreCase(input.substring(0, 6))) {
-				Service.deleteAccount(Integer.parseInt(input.substring(7)));
-			} else if ("add".equalsIgnoreCase(input.substring(0, 3))) {
-				String[] name = input.substring(4).split(", ");
-				Service.addAccount(name[0], name[1]);
-
-			} else if ("retrieve".equalsIgnoreCase(input.substring(0, 8))) {
-				System.out.println(Service.getAccountByAccountNumber(Integer.parseInt(input.substring(9))));
-			} else if (input.toLowerCase().startsWith("quit")) {
-				sc.close();
+			String i = scanner.nextLine();
+			if ("q".equalsIgnoreCase(i)) {
 				break;
-
-			} else {
-				System.out.println("Input not understood.");
 			}
+			Service.systemRun(i);
 
 		}
+		scanner.close();
 
 	}
-
 }
--- src/main/java/advanced/Service.java
+++ src/main/java/advanced/Service.java
@@ -2,15 +2,15 @@ package advanced;
 
 import java.util.HashMap;
 import java.util.Map;
+import org.json.JSONObject;
 
 public class Service {
-	
+
 	private static int accountNumber = 1;
 	private static Map<Integer, Account> accountList = new HashMap<>();
 
-	private Service() {}
-	
-
+	private Service() {
+	}
 
 	public static Account getAccountByAccountNumber(int accountNumber) {
 		return accountList.get(accountNumber);
@@ -26,9 +26,46 @@ public class Service {
 	public static void deleteAccount(int accountNumber) {
 		accountList.remove(accountNumber);
 	}
-	
+
 	public static String showAccount(int accountNumber) {
 		return accountNumber + ": " + getAccountByAccountNumber(accountNumber).toString();
-		
+
 	}
+
+	public static void getJSON() {
+		JSONObject json = new JSONObject(accountList);
+		System.out.println(json);
+	}
+	
+	public static void getJSON(int i) {
+		JSONObject json = new JSONObject(accountList.get(i));
+		System.out.println(json);
+	}
+
+
+	public static void systemRun(String input) {
+		switch (input.toLowerCase().substring(0, 1)) {
+		case "a":
+			Service.addAccount(input.substring(2).split(", ")[0], input.substring(2).split(", ")[1]);
+			break;
+		case "d":
+			Service.deleteAccount(Integer.valueOf(input.substring(2)));
+			break;
+		case "r":
+			System.out.println(Service.getAccountByAccountNumber(Integer.parseInt(input.substring(2))));
+			break;
+		case "j":
+			if (input.length() > 1) {
+				getJSON(Integer.valueOf(input.substring(2)));
+			} else {
+				getJSON();
+			}
+			break;
+		default:
+			System.out.println("Error");
+			break;
+
+		}
+	}
+
 }
