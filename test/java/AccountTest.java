2866c444c77a8d41c6f8208129d8a55cbc436ddd
WareJosephB <Ware.JosephB@Gmail.com>
2018-10-31 09:30:43 +0000

    Adding test that fails

---
+++ test/java/AccountTest.java
@@ -0,0 +1,22 @@
+import advanced.Service;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AccountTest {
+
+	@Test
+	public void test() {
+		
+		Service.addAccount("John", "Doe");
+		Service.addAccount("Joe", "Bloggs");
+		Service.addAccount("Steve", "Mnuchin");
+		Service.addAccount("John", "Brown");
+		Service.addAccount("Jon", "Bamber");
+		
+		int actual = Service.getCountByName("John")
+		
+		Assert.assertEquals(2, actual);
+		
+	}
+
+}
