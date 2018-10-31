b09d0cd8378ba39a9911001d7a9222673d04e12d
WareJosephB <Ware.JosephB@Gmail.com>
2018-10-31 09:38:46 +0000

    Refactoring to use Lambda functions

--- main/java/advanced/Service.java
+++ main/java/advanced/Service.java
@@ -4,13 +4,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Service {
-	
+
 	private static int accountNumber = 1;
 	private static Map<Integer, Account> accountList = new HashMap<>();
 
-	private Service() {}
-	
-
+	private Service() {
+	}
 
 	public static Account getAccountByAccountNumber(int accountNumber) {
 		return accountList.get(accountNumber);
@@ -26,23 +25,13 @@ public class Service {
 	public static void deleteAccount(int accountNumber) {
 		accountList.remove(accountNumber);
 	}
-	
+
 	public static String showAccount(int accountNumber) {
 		return accountNumber + ": " + getAccountByAccountNumber(accountNumber).toString();
-		
+
 	}
 
-
-
 	public static int getCountByName(String name) {
-		int count = 0;
-		int stepper = 1;
-		while (stepper < accountNumber) {
-			if (accountList.get(stepper).getFirstName().equals(name)) {
-				count ++;
-			}
-			stepper ++;
-		}
-		return count;
+		return (int) accountList.entrySet().stream().filter(w -> w.getValue().getFirstName() == name).count();
 	}
 }
